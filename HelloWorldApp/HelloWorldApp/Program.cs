a66f6e769509e460d31ebf9477cd09955ffa5aab
shariar-007 <uiu.151007@gmail.com>
2021-07-24 01:33:30 +0600

    practicing variable declearing

---
+++ HelloWorldApp/HelloWorldApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
